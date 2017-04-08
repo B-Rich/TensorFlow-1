a3fcc9dcc6188ddd4d0a83fc0c1dbcfd229b1277
Lewisxyz000 <kshr2d2@gmail.com>
2017-04-09 07:37:20 +0900

    Change Var name like #99 PR from hunkim/deelearningzerotoall

--- Lab03/Linear_Regression_Cost_Minimize_v1.0.py
+++ Lab03/Linear_Regression_Cost_Minimize_v1.0.py
@@ -14,14 +14,14 @@ sess = tf.Session()
 
 sess.run(tf.global_variables_initializer())
 
-w_record = []
-cost_record = []
+w_history = []
+cost_history = []
 
 for i in range(-30, 50):
     feed_w = i * 0.1
     curr_cost, curr_w = sess.run([cost, w], feed_dict={w: feed_w})
-    w_record.append(curr_w)
-    cost_record.append(curr_cost)
+    w_history.append(curr_w)
+    cost_history.append(curr_cost)
 
-plt.plot(w_record, cost_record)
+plt.plot(w_history, cost_history)
 plt.show()
